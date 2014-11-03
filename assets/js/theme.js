3ac9164710edfe4ba6ef63baff18731d534674b0
Noid <navid.saleh.n@gmail.com>
2014-11-03 23:13:57 +0330

    add projects

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -8,14 +8,20 @@ $(document).ready(function(){
       else
       {
             var active_div = $(this).parent().find(".page-box-active");
-            active_div.toggleClass('col-md-9 col-md-1').removeClass("page-box-active");
+            active_div.toggleClass('col-md-8 col-md-1').removeClass("page-box-active");
+            active_div.toggleClass('col-sm-8 col-sm-1').removeClass("page-box-active");
             active_div.find("#body").hide();
             active_div.find("#title").show();
-            $(this).toggleClass('col-md-1 col-md-9').addClass("page-box-active");
+            $(this).toggleClass('col-md-1 col-md-8').addClass("page-box-active");
+            $(this).toggleClass('col-sm-1 col-sm-8').addClass("page-box-active");
             $(this).find("#title").hide();
             $(this).find("#body").show();
         
       }
     });
-
+    window.onresize = function(event) {
+          if (window.outerWidth <= 750) {
+            document.location = "http://google.com";
+          }
+    };
 });
